93f682ea6390c46ff415b8e240369f979fd2e0f5
Jose Torres <jose.torres@jobsity.com>
2014-03-11 11:59:50 -0500

    Prevent undefined error when evaluating style prop

--- index.js
+++ index.js
@@ -10,37 +10,31 @@ document.ready = function () {
 
   var matcher = document.getElementsByClassName("bracket-matcher");
 
-  var divs = document.getElementsByTagName('div');
+  document.getElementsByClassName("overlayer")[0].onclick = function (e) {
+    if(typeof matcher[0] != 'undefined') {
+      if(hasClass(matcher[0].nextSibling, 'bracket-matcher')) {
+        var el = document.createElement("div");
 
-  for(var i = 0; i < divs.length; i++) {
-    divs[i].onclick = function(e) {
+        el.className = 'bracket-matcher-line';
+        el.style.position = 'relative';
+        el.style.display = 'block';
+        el.style.width = '7px';
+        el.style.borderRight = 'dotted 1px';
 
-      if(typeof matcher[0] != 'undefined') {
-        if(hasClass(matcher[0].nextSibling, 'bracket-matcher')) {
-          var el = document.createElement("div");
-          el.className = 'bracket-matcher-line';
-
-          el.style.position = 'relative';
-          el.style.display = 'block';
-          el.style.width = '7px';
-          el.style.borderRight = 'dotted 1px';
-
-          insertAfter(matcher[0], el);
-        }
+        insertAfter(matcher[0], el);
       }
+    }
 
-      var matcherLine = document.getElementsByClassName("bracket-matcher-line");
+    var matcherLine = document.getElementsByClassName("bracket-matcher-line");
+    matcherLine = matcherLine[0];
 
-      matcherLine = matcherLine[0];
-      if( parseInt(matcher[1].style.top) - parseInt(matcher[0].style.top) != 0 ) {
-        matcherLine.style.display = 'block';
-        matcherLine.style.height = (parseInt(matcher[1].style.top) - parseInt(matcher[0].style.top) - 15) + 'px';
-        matcherLine.style.top = (parseInt(matcher[0].style.top) + 15) + 'px';
-        matcherLine.style.left = (parseInt(matcher[1].style.left) - 3) + 'px';
-      } else {
-        matcherLine.style.display = 'none';
-      }
-
+    if( parseInt(matcher[1].style.top) - parseInt(matcher[0].style.top) != 0 ) {
+      matcherLine.style.display = 'block';
+      matcherLine.style.height = (parseInt(matcher[1].style.top) - parseInt(matcher[0].style.top) - 15) + 'px';
+      matcherLine.style.top = (parseInt(matcher[0].style.top) + 15) + 'px';
+      matcherLine.style.left = (parseInt(matcher[1].style.left) - 3) + 'px';
+    } else {
+      matcherLine.style.display = 'none';
     }
   }
 
